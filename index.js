1473e4d6e880da9055410be721a65bdd8651cdea
unknown <creativeapurba@gmail.com>
2023-07-17 20:58:23 +0530

    Create City Search

---
+++ index.js
@@ -0,0 +1,6 @@
+function search(){
+    const result = document.getElementById("result");
+    const h2 = document.createElement("h2");
+    h2.innerText = "27 Degree";
+    result.appendChild(h2);
+}
