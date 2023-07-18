4d6060987ecd9638a32791a1b08ae1415e9d0e6d
unknown <creativeapurba@gmail.com>
2023-07-18 21:36:32 +0530

    Add Weather Card

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function search(){
-    const result = document.getElementById("result");
-    const h2 = document.createElement("h2");
-    h2.innerText = "27 Degree";
-    result.appendChild(h2);
+
+    const searchCity = document.getElementById("search-city").value;
+    const city = document.getElementById("city");
+    city.innerText = searchCity;
+
 }
