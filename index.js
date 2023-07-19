9eccd0b5adc74a62a1b36bf82302716d89080de3
PritomChakrabarty <priyam.babusona@gmail.com>
2023-07-19 18:49:49 +0530

    Updation of Weather

--- index.js
+++ index.js
@@ -26,11 +26,13 @@ function search() {
     const searchCity = document.getElementById("search-city").value;
     let cityName;
     let temperature;
+    let weatherType;
     for (let i = 0; i < data.length; i++) {
         const cityData = data[i];
         if (cityData.city.toLowerCase() === searchCity.toLowerCase()) {
             cityName = cityData.city;
             temperature = cityData.temp;
+            weatherType = cityData.weather;
         }
     }
     const city = document.getElementById("city");
@@ -38,6 +40,9 @@ function search() {
 
     const temp = document.getElementById("temperature");
     temp.innerText = temperature;
+
+    const weather = document.getElementById("weatherType");
+    weather.innerText = weatherType;
 
     const today = new Date();
     const time = document.getElementById("time");
