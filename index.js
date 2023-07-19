42aa3ad11e3ac4b6aaeff6e83e214a13558a7e9b
unknown <creativeapurba@gmail.com>
2023-07-19 21:20:53 +0530

    Handle user error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function search() {
 
     const data = [
         {
-            city: "Kolkata", 
+            city: "Kolkata",
             temp: 28,
             weather: "Stormy"
         },
@@ -24,28 +24,41 @@ function search() {
     ]
 
     const searchCity = document.getElementById("search-city").value;
-    let cityName;
-    let temperature;
-    let weatherType;
-    for (let i = 0; i < data.length; i++) {
-        const cityData = data[i];
-        if (cityData.city.toLowerCase() === searchCity.toLowerCase()) {
-            cityName = cityData.city;
-            temperature = cityData.temp;
-            weatherType = cityData.weather;
+    const section = document.getElementById("weather-card")
+    if (searchCity != "") {
+        let cityName;
+        let temperature;
+        let weatherType;
+        let found = false;
+        for (let i = 0; i < data.length; i++) {
+            const cityData = data[i];
+            if (cityData.city.toLowerCase() === searchCity.toLowerCase()) {
+                found = true;
+                cityName = cityData.city;
+                temperature = cityData.temp;
+                weatherType = cityData.weather;    
+                section.style.display = "block";
+            }
         }
+        if(found === false){
+            alert("City not found");
+            section.style.display = "none";
+        }
+        const city = document.getElementById("city");
+        city.innerText = cityName;
+
+        const temp = document.getElementById("temperature");
+        temp.innerText = temperature;
+
+        const weather = document.getElementById("weatherType");
+        weather.innerText = weatherType;
+
+        const today = new Date();
+        const time = document.getElementById("time");
+        time.innerText = today.getHours() + ":" + today.getMinutes()
     }
-    const city = document.getElementById("city");
-    city.innerText = cityName;
-
-    const temp = document.getElementById("temperature");
-    temp.innerText = temperature;
-
-    const weather = document.getElementById("weatherType");
-    weather.innerText = weatherType;
-
-    const today = new Date();
-    const time = document.getElementById("time");
-    time.innerText = today.getHours() + ":" + today.getMinutes()
-
+    else {
+        alert("Please enter a city name");
+        section.style.display = "none";
+    }
 }
